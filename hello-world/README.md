6be1ac59e3009c6e8764c54086d645532806cd28
mary-metilda <mary.metilda@vonnue.com>
2024-03-04 10:29:08 +0530

    intro to vue 3

---
+++ hello-world/README.md
@@ -0,0 +1,8 @@
+# Commands to start a project
+
+1. npm -g add vue/cli
+2. vue create hello-world
+3. cd hello-world
+4. npm run serve
+
+
---
+++ hello-world/src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <!--text interpolation-->
+  <h3>{{ name }}</h3>
+  <!--text binding using directive-->
+  <div v-text="description"></div>
+  <div v-html="subTitle"></div>
+  <div v-html="hack"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underlined Text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 :class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
+  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
+  <h2 :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+    Array conditional movie
+  </h2>
+  <h2
+    :class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut,
+    }"
+  >
+    Object conditional movie
+  </h2>
+  <h2
+    :style="{
+      color: highlightColor,
+      'font-size': headerSize + 'px',
+      padding: '20px',
+    }"
+  >
+    Inline style
+  </h2>
+  <h2 :style="headerStyleObject">Style Object</h2>
+  <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
+  <div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Sample Name",
+      description: "Sample Description",
+      subTitle: "<b>Sample SubTitle<b>",
+      hack: `<a href="#" onClick="alert('You have been hacked!')"> Win a prize! <a/>`,
+      headingId: "heading",
+      isDisabled: false,
+      status: "danger",
+      isPromoted: true,
+      isSoldOut: true,
+      highlightColor: "orange",
+      headerSize: 50,
+      headerStyleObject: {
+        color: "orange",
+        fontSize: "50px",
+        padding: "20px",
+      },
+      baseStyleObject: {
+        fontSize: "50px",
+        padding: "10px",
+      },
+      successStyleObject: {
+        color: "green",
+        backgroundColor: "lightgreen",
+        border: "1px solid green",
+        padding: "20px",
+      },
+      dangerStyleObject: {
+        color: "darkred",
+        backgroundColor: "red",
+        border: "1px solid darkred",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: green;
+}
+
+.sold-out {
+  color: red;
+}
+</style>
