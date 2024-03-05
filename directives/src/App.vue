abaf098464a18e8a7f9539b0e5e0317cb0cccdfe
mary-metilda <mary.metilda@vonnue.com>
2024-03-05 10:02:36 +0530

    feat: directives

---
+++ directives/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2 v-once>{{ name }}</h2>
+  <button @click="name = 'Batman'">Change name</button>
+  <h2 v-pre>{{ name }}</h2>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Metilda",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
