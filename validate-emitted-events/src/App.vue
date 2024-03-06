2364bb40c7d70b629a31292af221616761616877
mary-metilda <mary.metilda@vonnue.com>
2024-03-06 10:15:49 +0530

    feat: validate emitted events

---
+++ validate-emitted-events/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="showPopup = true">showPopup</button>
+  <PopUp v-show="showPopup" @close="closePopup" />
+</template>
+
+<script>
+import PopUp from "./components/PopUp";
+
+export default {
+  name: "App",
+  components: {
+    PopUp,
+  },
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
+  methods: {
+    closePopup(name) {
+      (this.showPopup = false), console.log("name", name);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
