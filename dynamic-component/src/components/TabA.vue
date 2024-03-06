00f8c57af39f276c7ad1dc87b07e0f5e85addba3
mary-metilda <mary.metilda@vonnue.com>
2024-03-06 15:22:06 +0530

    feat: dynamic components

---
+++ dynamic-component/src/components/TabA.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Tab A content</h2>
+</template>
+
+<script>
+export default {
+  name: "TabA",
+};
+</script>
+
+<style scoped></style>
---
+++ dynamic-component/src/components/TabB.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Tab B content</h2>
+</template>
+
+<script>
+export default {
+  name: "TabB",
+};
+</script>
+
+<style scoped></style>
