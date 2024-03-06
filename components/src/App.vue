f88e56fba3905ecd661f2eabc29dc4a57e06cf6c
mary-metilda <mary.metilda@vonnue.com>
2024-03-06 09:11:20 +0530

    feat: components

---
+++ components/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- <GreetHello name="Bruce" heroName="batman" />
+  <GreetHello name="Clark" heroName="superman" />
+  <GreetHello name="Diana" heroName="wonder woman" />
+  <GreetHello :name="name" :heroName="description" /> -->
+  <article-compornet id="my-article" title="Article Title" :likes="50" :isPublished="true" />
+</template>
+
+<script>
+// import GreetHello from "./components/GreetHello.vue";
+import ArticleCompornet from "./components/ArticleComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    // GreetHello,
+    ArticleCompornet,
+  },
+  data() {
+    return {
+      name: "Sample Name",
+      description: "Sample Description",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ components/src/components/GreetHello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Hello {{ name }} a.k.a {{ heroName }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GreetHello",
+  props: ["name", "heroName"],
+};
+</script>
+
+<style scoped></style>
