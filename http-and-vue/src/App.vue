255245432a4ce4e172673e4726d2214b4164a087
mary-metilda <mary.metilda@vonnue.com>
2024-03-06 15:58:42 +0530

    feat: teleport

---
+++ http-and-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Teleport to="#portal-root ">
+    <PortalComponent />
+  </Teleport>
+</template>
+
+<script>
+import PortalComponent from "./components/PortalComponent";
+
+export default {
+  name: "App",
+  components: {
+    PortalComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
