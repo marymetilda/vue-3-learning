32e60f6231c936a1db84d4b018c160b3ec4a3f61
mary-metilda <mary.metilda@vonnue.com>
2024-03-06 10:55:02 +0530

    feat: emit events

---
+++ slots/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <InputComponent v-model="name"/>
+  <!-- <input type="text" v-model="name" /> -->
+</template>
+
+<script>
+import InputComponent from "./components/InputComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    InputComponent,
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
