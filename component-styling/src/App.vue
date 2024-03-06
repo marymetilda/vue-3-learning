b40cf7b9573a942b7400f1e44b45fae10e570a05
mary-metilda <mary.metilda@vonnue.com>
2024-03-06 14:21:32 +0530

    feat: component styling

---
+++ component-styling/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h4>App component text</h4>
+  <ChildStyles />
+  <TestSection>
+    <h4>Test section</h4>
+  </TestSection>
+</template>
+
+<script>
+import ChildStyles from "./components/ChildStyles.vue";
+import TestSection from "./components/TestSection.vue";
+
+export default {
+  name: "App",
+  components: {
+    ChildStyles,
+    TestSection
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h4{
+    color: blueviolet;
+}
+</style>
---
+++ component-styling/src/components/ChildStyles.vue
@@ -0,0 +1,15 @@
+<template>
+<h4>ChildStyles component text</h4>
+</template>
+
+<script>
+export default {
+  name: "ChildStyles",
+};
+</script>
+
+<style scoped>
+h4{
+    color: olivedrab;
+}
+</style>
