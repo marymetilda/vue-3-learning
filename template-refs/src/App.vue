92afa93b90fd510f1dc2a94d3d0dd3faa0186fa6
mary-metilda <mary.metilda@vonnue.com>
2024-03-07 10:28:20 +0530

    feat: template refs

---
+++ template-refs/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TemplateRef />
+</template>
+
+<script>
+import TemplateRef from "./components/TemplateRef.vue";
+
+export default {
+  name: "App",
+  components: {
+    TemplateRef,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ template-refs/src/components/PostList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>post list component</div>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+};
+</script>
+
+<style scoped></style>
