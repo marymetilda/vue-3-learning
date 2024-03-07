5c724c57e960b4781fe8104fe68645722d599dcf
mary-metilda <mary.metilda@vonnue.com>
2024-03-07 16:19:36 +0530

    feat: composition api

---
+++ composition-api/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <MethodsComponent />
+  <VModel />
+  <ComputedComponent />
+</template>
+
+<script>
+import MethodsComponent from "./components/MethodsComponent";
+import VModel from "./components/VModel";
+import ComputedComponent from "./components/ComputedComponent";
+
+export default {
+  name: "App",
+  components: {
+    MethodsComponent,
+    VModel,
+    ComputedComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
