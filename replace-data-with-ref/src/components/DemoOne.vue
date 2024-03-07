1458cd681aa447e8db6d9d0f879715285a2b3f1e
mary-metilda <mary.metilda@vonnue.com>
2024-03-07 15:31:40 +0530

    feat: replace data with ref

---
+++ replace-data-with-ref/src/components/DemoOne.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>
+    {{ name }}
+  </h2>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "DemoOne",
+  setup() {
+    const name = ref("Sample Name");
+
+    setTimeout(() => {
+      name.value = "Test name";
+    }, 2000);
+
+    return {
+      name,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ replace-data-with-ref/src/components/DemoTwo.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>{{ state.firstName }} {{ state.lastName }}</h2>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  name: "DemoTwo",
+  setup() {
+    const state = reactive({
+      firstName: "Bruce",
+      lastName: "Wayne",
+    });
+
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
