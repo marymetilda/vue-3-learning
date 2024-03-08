b7d8f780e2b6e5ec5b0360380a605eca6a36712f
mary-metilda <mary.metilda@vonnue.com>
2024-03-08 12:07:15 +0530

    feat: props composition api

---
+++ props-composition-api/src/components/PersonGreeting.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>Hello {{ fullName }}</div>
+  <button @click="sendEvent">Call Heroes</button>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "PersonGreeting",
+  setup(props, context) {
+    const fullName = computed(() => {
+      return `${props.firstName} ${props.lastName}`;
+    });
+
+    function sendEvent() {
+      context.emit("callHeroes", fullName.value);
+    }
+
+    return { fullName, sendEvent };
+  },
+  props: ["firstName", "lastName"],
+  emits: ["callHeroes"],
+  //   computed: {
+  //     fullName() {
+  //       return `${this.firstName} ${this.lastName}`;
+  //     },
+  //   },
+};
+</script>
+
+<style scoped></style>
